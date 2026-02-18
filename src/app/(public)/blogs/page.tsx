9600cba956f59b3e91feb1493ae34cbe2b7bfacd
Debos Das <debos.das.02@gmail.com>
2026-02-19 00:56:03 +0600

    added blog post api

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,7 +1,21 @@
-const AllBlogsPage = () => {
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+import { Post } from "@/types";
+
+const AllBlogsPage = async () => {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const { data: blogs } = await res.json();
+  console.log(blogs);
   return (
-    <div className="py-30 px-4 max-w-7xl mx-auto">
+    <div className="py-30 px-4 max-w-7xl mx-auto my-5">
       <h2 className="text-center text-4xl">All Blogs</h2>
+      <div className="grid grid-cols-3 gap-3  max-w-6xl  mx-auto">
+        {
+          blogs.map((blog: Post) => {
+            <BlogCard key={blog.id} post={blog}></BlogCard>;
+          })
+        }
+      </div>
     </div>
   );
 };
--- src/components/modules/Blogs/BlogCard.tsx
+++ src/components/modules/Blogs/BlogCard.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Link from "next/link";
 import Image from "next/image";
+import { Post } from "@/types";
 
-export default function BlogCard({ post }: { post: any }) {
+export default function BlogCard({ post }: { post: Post; }) {
   return (
     <Link
       href={`/blogs/${post.id}`}
@@ -37,7 +38,7 @@ export default function BlogCard({ post }: { post: any }) {
             <div className="flex items-center gap-2">
               <Image
                 src={
-                  post.author.picture ||
+                  post.author?.picture ||
                   "https://cdn-icons-png.flaticon.com/512/9385/9385289.png"
                 }
                 alt={post.author.name}
@@ -47,7 +48,7 @@ export default function BlogCard({ post }: { post: any }) {
               />
               <span className="text-gray-600 dark:text-gray-300 text-sm flex items-center gap-1">
                 {post.author.name}
-                {post.author.isVerified && (
+                {post.author?.isVerified && (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-4 w-4 text-blue-500"
